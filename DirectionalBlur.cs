84b166ed62903876f7d84fec3c54a4d677d7034a
Nobuyuki Nakata <nobu.nakata@gmail.com>
2017-07-28 17:33:26 +0900

    ネームスペース追加

--- DirectionalBlur.cs
+++ DirectionalBlur.cs
@@ -2,68 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode]
-public class DirectionalBlur : MonoBehaviour {
-    const string PROP_AMOUNT_TEX = "_AmountTex";
-    const string PROP_DIRECTION  ="_Dir";
-    const string PROP_DISTANCE_STRETCH = "_StretchDist";
+namespace DirectionalBlurPackage {
 
-    [SerializeField]
-    Shader shader;
-    [SerializeField]
-    Texture amountTex;
+    [ExecuteInEditMode]
+    public class DirectionalBlur : MonoBehaviour {
+        const string PROP_AMOUNT_TEX = "_AmountTex";
+        const string PROP_DIRECTION  ="_Dir";
+        const string PROP_DISTANCE_STRETCH = "_StretchDist";
 
-    [SerializeField]
-    [Range(1f, 10f)]
-    int iterations = 1;
-    [SerializeField]
-    [Range(0f, 1f)]
-    float stretchDist = 1f;
-    [SerializeField]
-    [Range(0f, 1f)]
-    float direction = 0f;
+        [SerializeField]
+        Shader shader;
+        [SerializeField]
+        Texture amountTex;
 
-    Material mat;
-    DepthTextureMode lastDepthTexMode;
+        [SerializeField]
+        [Range(1f, 10f)]
+        int iterations = 1;
+        [SerializeField]
+        [Range(0f, 1f)]
+        float stretchDist = 1f;
+        [SerializeField]
+        [Range(0f, 1f)]
+        float direction = 0f;
 
-    #region Unity
-    void OnEnable() {
-        mat = new Material (shader);
-    }
-    void OnPreRender() {
-        var c = Camera.current;
-        lastDepthTexMode = c.depthTextureMode;
-        c.depthTextureMode = DepthTextureMode.Depth;
-    }
-    void OnRenderImage(RenderTexture src, RenderTexture dst) {
-        mat.SetTexture (PROP_AMOUNT_TEX, amountTex);
-        mat.SetFloat (PROP_DIRECTION, direction);
-        mat.SetFloat (PROP_DISTANCE_STRETCH, stretchDist);
+        Material mat;
+        DepthTextureMode lastDepthTexMode;
 
-        var tmp0 = RenderTexture.GetTemporary (src.width, src.height, 0, src.format);
-        var tmp1 = RenderTexture.GetTemporary (src.width, src.height, 0, src.format);
-
-        Graphics.Blit (src, tmp0);
-        for (var i = 0; i < iterations; i++) {
-            Graphics.Blit (tmp0, tmp1, mat);
-            var tmp = tmp0;
-            tmp0 = tmp1;
-            tmp1 = tmp;
+        #region Unity
+        void OnEnable() {
+            mat = new Material (shader);
         }
-        Graphics.Blit (tmp0, dst);
+        void OnPreRender() {
+            var c = Camera.current;
+            lastDepthTexMode = c.depthTextureMode;
+            c.depthTextureMode = DepthTextureMode.Depth;
+        }
+        void OnRenderImage(RenderTexture src, RenderTexture dst) {
+            mat.SetTexture (PROP_AMOUNT_TEX, amountTex);
+            mat.SetFloat (PROP_DIRECTION, direction);
+            mat.SetFloat (PROP_DISTANCE_STRETCH, stretchDist);
 
-        RenderTexture.ReleaseTemporary (tmp0);
-        RenderTexture.ReleaseTemporary (tmp1);
-    }
-    void OnPostRender() {
-        Camera.current.depthTextureMode = lastDepthTexMode;
-    }
-    void OnDisable() {
-        if (Application.isPlaying)
-            Destroy (mat);
-        else
-            DestroyImmediate (mat);
-    }
-    #endregion
+            var tmp0 = RenderTexture.GetTemporary (src.width, src.height, 0, src.format);
+            var tmp1 = RenderTexture.GetTemporary (src.width, src.height, 0, src.format);
 
+            Graphics.Blit (src, tmp0);
+            for (var i = 0; i < iterations; i++) {
+                Graphics.Blit (tmp0, tmp1, mat);
+                var tmp = tmp0;
+                tmp0 = tmp1;
+                tmp1 = tmp;
+            }
+            Graphics.Blit (tmp0, dst);
+
+            RenderTexture.ReleaseTemporary (tmp0);
+            RenderTexture.ReleaseTemporary (tmp1);
+        }
+        void OnPostRender() {
+            Camera.current.depthTextureMode = lastDepthTexMode;
+        }
+        void OnDisable() {
+            if (Application.isPlaying)
+                Destroy (mat);
+            else
+                DestroyImmediate (mat);
+        }
+        #endregion
+    }
 }
